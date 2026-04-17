d766a8d46d9498697b9202119223ea2d1e095994
Marco Salvi <marco.salvi@ingv.it>
2026-04-17 10:36:36 +0200

    enable facilities in the oss version

--- src/environments/environment.opensource.ts
+++ src/environments/environment.opensource.ts
@@ -24,7 +24,7 @@ export const environment = {
     modules: {
       data: true,
       analysis: false,
-      registry: false,
+      registry: true,
       software: true
     },
     mainMenu: [
