1e6a832564259c06c9e6242df8e91945091fba90
Valerio Vinciarelli <valerio.vinciarelli@gmail.com>
2026-09-17 11:19:46 +0200

    update euro

--- src/utility/eposLeaflet/components/layerControlComponent/baseMapSelectorOptionsComponent/basemapSelectorOptions.component.ts
+++ src/utility/eposLeaflet/components/layerControlComponent/baseMapSelectorOptionsComponent/basemapSelectorOptions.component.ts
@@ -108,7 +108,7 @@ export class BasemapSelectorOptionsComponent implements OnChanges {
   }
 
   private createLayer(option: EuroGeographicsMapOption): WmsTileLayer {
-    const parsedUrl = new URL(option.wmsUrl);
+    const parsedUrl = new URL(option.wmsUrl, window.location.origin);
     const serviceUrl = `${parsedUrl.origin}${parsedUrl.pathname}`;
 
     const layer = new WmsTileLayer(this.getLayerId(option), option.name)
